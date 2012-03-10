171b8b54ccf36e626052149f480d339b2ae9e5f3
Patric Mueller <bhaak@gmx.net>
2012-03-10 11:33:14 +0100

    Filter files by ending

--- src/com/github/bhaak/HackedUpReaderExtension.java
+++ src/com/github/bhaak/HackedUpReaderExtension.java
@@ -18,8 +18,9 @@ public class HackedUpReaderExtension implements Menuable {
 		// HackedUpReader location
 		File cr3 = new File("/mnt/us/cr3xcb/bin/cr3");
 
+		// e-books location
 		File folder = new File("/mnt/us/documents/");
-		File[] files = folder.listFiles();
+		File[] files = folder.listFiles(new BookFilter());
 		for (int i=0; i<files.length; i++) {
 			if (files[i].isFile()) {
 				// add all e-books under folder
@@ -30,10 +31,30 @@ public class HackedUpReaderExtension implements Menuable {
 						files[i].getAbsolutePath());
 				topMenu.addMenuItem(launcherScript);
 			} else if (files[i].isDirectory()) {
+				// add sub directories as sub menus
 				LauncherAction subMenu = new LauncherMenu(files[i].getName(), i);
 				topMenu.addMenuItem(subMenu);
 				// TODO rekursiv runter
 			}
+		}
+	}
+
+	class BookFilter implements java.io.FileFilter {
+		@Override
+		public boolean accept(File file) {
+			String filename = file.getName();
+			if (filename.toLowerCase().endsWith(".epub") ||
+				filename.toLowerCase().endsWith(".fb2") ||
+				filename.toLowerCase().endsWith(".txt") ||
+				filename.toLowerCase().endsWith(".rtf") ||
+				filename.toLowerCase().endsWith(".html") ||
+				filename.toLowerCase().endsWith(".htm") ||
+				filename.toLowerCase().endsWith(".pdb")) {
+				return true;
+			} else if (file.isDirectory()) {
+				return true;
+			}
+			return false;
 		}
 	}
 }
